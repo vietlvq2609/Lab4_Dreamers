3bee464805057ddd671ab87a3118e3c9626c35c5
Linh Tran <linhtn0211@gmail.com>
2023-12-16 05:53:34 +0700

    Cau 1

---
+++ Controllers/SuppliersController.cs
@@ -0,0 +1,15 @@
+﻿using Lab4_Dreamers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4_Dreamers.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        public ActionResult Index(int choice = 0)
+        {
+            var db = new DbContext();
+            var suppliers = db.GetSuppliers(choice);
+            return View(suppliers);
+        }
+    }
+}
--- DbContext.cs
+++ DbContext.cs
@@ -7,7 +7,8 @@ namespace Lab4_Dreamers
 {
     public class DbContext
     {
-        private string connectionString = "Server=(localdb)\\mssqllocaldb;Database=NorthWind;Trusted_Connection=True;";
+        
+        private string connectionString = "server=DESKTOP-J648SAE; database=NorthWind; Trusted_connection=True; Encrypt=False";
         public DbContext()
         {
         }
@@ -20,7 +21,7 @@ namespace Lab4_Dreamers
                 connection.Open();
 
                 string query = "SELECT * FROM Suppliers";
-
+                               
                 using (SqlCommand command = new(query, connection))
                 {
                     using (SqlDataReader reader = command.ExecuteReader())
@@ -32,6 +33,9 @@ namespace Lab4_Dreamers
                                 SupplierID = reader.GetInt32(reader.GetOrdinal("SupplierID")),
                                 CompanyName = reader.GetString(reader.GetOrdinal("CompanyName")),
                                 ContactName = reader.GetString(reader.GetOrdinal("ContactName")),
+                                Address = reader.GetString(reader.GetOrdinal("Address")),
+                                City = reader.GetString(reader.GetOrdinal("City")),
+                                Country = reader.GetString(reader.GetOrdinal("Country")),
                             };
 
                             suppliers.Add(supplier);
@@ -39,9 +43,53 @@ namespace Lab4_Dreamers
                     }
                 }
             }
-
             return suppliers;
         }
+
+        public List<Supplier> GetSuppliers(int choice=0)
+        {
+            List<Supplier> suppliers = new();
+
+            using (SqlConnection connection = new SqlConnection(this.connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM Suppliers";
+
+                if(choice == 1)
+                {
+                    query = "SELECT * FROM Suppliers " +
+                               "WHERE SupplierID IN " +
+                               "(SELECT SupplierID FROM Products GROUP BY SupplierID HAVING COUNT(ProductID) >= 10)";
+                }else if(choice ==2)
+                {
+                    query = "SELECT * FROM Suppliers " +
+                               "WHERE SupplierID IN " +
+                               "(SELECT SupplierID FROM Products GROUP BY SupplierID HAVING COUNT(ProductID) = 0)";
+                }
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Supplier supplier = new Supplier
+                            {
+                                SupplierID = (int)reader["SupplierID"],
+                                CompanyName = reader["CompanyName"].ToString(),
+                                ContactName = reader["ContactName"].ToString(),
+                                Address = reader["Address"].ToString(),
+                                City = reader["City"].ToString(),
+                                Country = reader["Country"].ToString()
+                            };
+
+                            suppliers.Add(supplier);
+                        }
+                    }
+                }
+            }
+            return suppliers;
+        }
+
         public List<Product> GetProducts(string comName="",string proName="")
         {
             List<Product> products = new();
